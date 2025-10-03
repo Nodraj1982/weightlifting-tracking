eab9a709455334787226b828ea0f83c71da48368
Kenne Kennedy <kennedy24812025@outlook.com>
2025-10-03 15:33:03 +0100

    Implement db functions and update requirements

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import os
 import requests
 import streamlit as st
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 def login_user(email, password):
     if not SUPABASE_URL or not SUPABASE_KEY:
